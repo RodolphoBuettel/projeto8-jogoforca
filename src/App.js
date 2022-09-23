4993b7d462b7dab67542be1e2abe91d44d019b82
Rodolpho Buettel <rodolfo@DESKTOP-C9SP90O.localdomain>
2022-09-23 15:29:02 -0300

    Escolher palavra e botões funcionando

--- src/App.js
+++ src/App.js
@@ -11,32 +11,76 @@ import forca4 from "./img/forca4.png";
 import forca5 from "./img/forca5.png";
 import forca6 from "./img/forca6.png";
 
+import palavras from "./Palavras";
 
-function Teclado(props){
-    return(
-        <button className="letra">{props.l.toUpperCase()}</button>
-        );
+
+function Teclado(props) {
+    const [habilitaTeclado, sethabilitaTeclado] = React.useState(false);
+    const [cor, setcor] = React.useState("habilitado");
+
+    function EsoclherLetra(){
+        sethabilitaTeclado(true);
+        const mudaCor = "";
+        setcor(mudaCor);
+        
+    }
+        if(props.desabilitaButton === true){
+           return( <button disabled = {habilitaTeclado} className={`letra ${cor}`} onClick =
+            {EsoclherLetra}>{props.l.toUpperCase()}</button>);
+        }
+        else{
+            return(<button disabled className={`letra`}>{props.l.toUpperCase()}</button>);
+        }     
+        
 }
 
 
 export default function App() {
 
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const [renderizarLinhnas, setrenderizarLinhas] = React.useState([]);
+   
+    const [desabilitaButton, setdesabilitaButton] = React.useState(false);
+    const [erro, seterro] = React.useState(0);
 
+    function EscolherPalavra() {
+
+        const palavraSelecionada = palavras[Math.floor(Math.random() * palavras.length)];
+        console.log(palavraSelecionada);
+        let letrasDaPalavra = palavraSelecionada.split('');
+        console.log(letrasDaPalavra);
+
+        const traco = "_ ";
+        let linhas = [];
+        for (let i = 0; i < letrasDaPalavra.length; i++) {
+            linhas.push(traco);
+        }
+        setrenderizarLinhas(linhas);
+
+        const desablita = true;
+        setdesabilitaButton(desablita);
+
+        console.log(linhas);
+    }
     return (
-        <div className="jogo">   
-        <div><img src={forca0} /></div>
-        <div className="escolherPalavra">
-            <button className="escolha">Escolher Palavra</button>
-        </div>
-        <div className="teclado">
-            {alfabeto.map((l) => <Teclado l = {l}/>)}
-        </div>
-        <div className="chute">
-            <a className="negrito">Já sei a palavra!</a>
-            <input></input>
-            <button className="">Chutar</button>
-        </div>
+        <div className="jogo">
+            <div>
+                <img src={forca0} />
+            </div>
+            <div className="escolherPalavra">
+                <button disabled = {desabilitaButton} className="escolha" onClick={EscolherPalavra}>Escolher Palavra</button>
+            </div>
+            <div className="teclado">
+                {alfabeto.map((l, letra) => <Teclado key={letra} l={l} desabilitaButton = {desabilitaButton} />)}
+            </div>
+            <div className="chute">
+                <a className="negrito">Já sei a palavra!</a>
+                <input></input>
+                <button className="">Chutar</button>
+            </div>
+            <div className="underline">
+                <a className="linhas">{renderizarLinhnas}</a>
+            </div>
         </div>
     )
 }
