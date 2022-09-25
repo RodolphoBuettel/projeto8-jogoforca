45a76c2e8553b314605d081bbbcd74f4c966e052
Rodolpho Buettel <rodolfo@DESKTOP-C9SP90O.localdomain>
2022-09-25 20:33:02 -0300

    bug

--- src/App.js
+++ src/App.js
@@ -14,64 +14,110 @@ import forca6 from "./img/forca6.png";
 import palavras from "./Palavras";
 
 
-function Teclado(props) {
-    const [habilitaTeclado, sethabilitaTeclado] = React.useState(false);
-    const [cor, setcor] = React.useState("habilitado");
 
-    function EsoclherLetra(){
-        sethabilitaTeclado(true);
-        const mudaCor = "";
-        setcor(mudaCor);
-        
-    }
-        if(props.desabilitaButton === true){
-           return( <button disabled = {habilitaTeclado} className={`letra ${cor}`} onClick =
-            {EsoclherLetra}>{props.l.toUpperCase()}</button>);
-        }
-        else{
-            return(<button disabled className={`letra`}>{props.l.toUpperCase()}</button>);
-        }     
-        
-}
 
 
 export default function App() {
-
+    const [linhas, setlinhas] = React.useState([]);
+    console.log(linhas);
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    const [renderizarLinhnas, setrenderizarLinhas] = React.useState([]);
-   
     const [desabilitaButton, setdesabilitaButton] = React.useState(false);
-    const [erro, seterro] = React.useState(0);
-
+    const [palavraSelecionada, setpalavraSelecionada] = React.useState([]);
+    const [erro, seterro] = React.useState(1);
+    const [fase, setfase] = React.useState(forca0);
+    console.log(erro);
+    console.log(fase);
+    let arrayNovo = [];
+    let array = [];
+    
+    console.log(palavraSelecionada);
+    
     function EscolherPalavra() {
-
-        const palavraSelecionada = palavras[Math.floor(Math.random() * palavras.length)];
-        console.log(palavraSelecionada);
-        let letrasDaPalavra = palavraSelecionada.split('');
-        console.log(letrasDaPalavra);
-
-        const traco = "_ ";
-        let linhas = [];
-        for (let i = 0; i < letrasDaPalavra.length; i++) {
+        
+        const escolhida = palavras[Math.floor(Math.random() * palavras.length)];
+        setpalavraSelecionada([...escolhida]);
+        const traco = "_";
+        for (let i = 0; i < [...escolhida].length; i++) {
             linhas.push(traco);
         }
-        setrenderizarLinhas(linhas);
-
+        arrayNovo = linhas;
+        setlinhas(arrayNovo);
+        
         const desablita = true;
         setdesabilitaButton(desablita);
-
-        console.log(linhas);
+        
     }
+    
+    function Teclado(props) {
+        const [habilitaTeclado, sethabilitaTeclado] = React.useState(false);
+        const [cor, setcor] = React.useState("habilitado");
+        
+        function EscolherLetra(l) {
+            sethabilitaTeclado(true);
+
+            const mudaCor = "";
+            setcor(mudaCor);
+
+            palavraSelecionada.forEach((elemento, indice) => {
+                if (elemento === l) {
+                    linhas[indice] = palavraSelecionada[indice];
+                }
+                return linhas[indice];
+            });
+            if(!palavraSelecionada.includes(l)){
+                console.log("errou");
+                 seterro(erro + 1); 
+                if(erro === 1){
+                    setfase(forca1);
+                }
+                if(erro === 2){
+                    setfase(forca2);
+                }
+                if(erro === 3){
+                    setfase(forca3);
+                }
+                if(erro === 4){
+                    setfase(forca4);
+                }
+                if(erro === 5){
+                    setfase(forca5);
+                }
+                if(erro === 6){
+                    setfase(forca6);
+                }
+                if(erro === 7){
+                    alert("perdeu");
+                    setlinhas(palavraSelecionada);
+                }
+            }
+            array = linhas;
+            setlinhas(array);
+            console.log(array);
+           
+        }
+        
+        if (props.desabilitaButton === true) {
+            return (<button disabled={habilitaTeclado} className={`letra ${cor}`} onClick=
+                {() => EscolherLetra(props.l)}>{props.l}</button>);
+        }
+        else {
+            return (<button disabled className={`letra`}>{props.l}</button>);
+        }
+    
+    }
+
+
     return (
         <div className="jogo">
             <div>
-                <img src={forca0} />
+                <img src={fase} />
             </div>
             <div className="escolherPalavra">
-                <button disabled = {desabilitaButton} className="escolha" onClick={EscolherPalavra}>Escolher Palavra</button>
+                <button disabled={desabilitaButton} className="escolha" onClick={EscolherPalavra}>Escolher Palavra</button>
             </div>
             <div className="teclado">
-                {alfabeto.map((l, letra) => <Teclado key={letra} l={l} desabilitaButton = {desabilitaButton} />)}
+                {alfabeto.map((l, letra) => <Teclado key={letra} l={l} desabilitaButton={desabilitaButton}
+                      />)}
             </div>
             <div className="chute">
                 <a className="negrito">Já sei a palavra!</a>
@@ -79,7 +125,7 @@ export default function App() {
                 <button className="">Chutar</button>
             </div>
             <div className="underline">
-                <a className="linhas">{renderizarLinhnas}</a>
+                <a className="linhas">{linhas}</a>
             </div>
         </div>
     )
