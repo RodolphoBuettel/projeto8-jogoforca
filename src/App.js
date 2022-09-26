a50e373d1d8a627d87ccf73b97c10f7ea44c4e78
Rodolpho Buettel <rodolfo@DESKTOP-C9SP90O.localdomain>
2022-09-26 14:23:39 -0300

    Bugs resolvidos e jogo completo

--- src/App.js
+++ src/App.js
@@ -12,100 +12,134 @@ import forca5 from "./img/forca5.png";
 import forca6 from "./img/forca6.png";
 
 import palavras from "./Palavras";
+let arrayDeLinhas = [];
+let arrayDeLetrasTrocadas = [];
 
+function Teclado(props) {
+    const [habilitaTeclado, sethabilitaTeclado] = React.useState(false);
+    const [cor, setcor] = React.useState("habilitado");
 
+    function EscolherLetra(l) {
+        sethabilitaTeclado(true);
 
+        const mudaCor = "";
+        setcor(mudaCor);
+
+        props.palavra.forEach((elemento, indice) => {
+            if (elemento === l) {
+                props.linhas[indice] = props.palavra[indice];
+            }
+            return props.linhas[indice];
+        });
+        if (!props.palavra.includes(l)) {
+            props.seterro(props.erro + 1);
+            if (props.erro === 1) {
+                props.setfase(forca1);
+            }
+            if (props.erro === 2) {
+                props.setfase(forca2);
+            }
+            if (props.erro === 3) {
+                props.setfase(forca3);
+            }
+            if (props.erro === 4) {
+                props.setfase(forca4);
+            }
+            if (props.erro === 5) {
+                props.setfase(forca5);
+            }
+            if (props.erro === 6) {
+                props.setfase(forca6);
+                alert("perdeu");
+                props.setvermelho("vermelho");
+                props.setdesabilitaButton(false);
+                return props.setlinhas([...props.palavra]);
+            }
+
+        }
+        arrayDeLetrasTrocadas = [...props.linhas];
+        props.setlinhas(arrayDeLetrasTrocadas);
+        let j = 0;
+        for (let i = 0; i < props.palavra.length; i++) {
+            if (props.linhas[i] === props.palavra[i]) {
+                j++;
+            }
+            if (j === props.palavra.length) {
+                props.setdesabilitaButton(false);
+                props.setvermelho("verde");
+                alert("ganhou");
+            }
+        }
+    }
+
+    if (props.desabilitaButton === true) {
+        return (<button disabled={habilitaTeclado} className={`letra ${cor}`} onClick=
+            {() => EscolherLetra(props.l)}>{props.l.toUpperCase()}</button>);
+    }
+    else {
+        return (<button disabled className={`letra`}>{props.l.toUpperCase()}</button>);
+    }
+
+}
 
 
 export default function App() {
     const [linhas, setlinhas] = React.useState([]);
-    console.log(linhas);
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     const [desabilitaButton, setdesabilitaButton] = React.useState(false);
     const [palavraSelecionada, setpalavraSelecionada] = React.useState([]);
     const [erro, seterro] = React.useState(1);
     const [fase, setfase] = React.useState(forca0);
-    console.log(erro);
-    console.log(fase);
-    let arrayNovo = [];
-    let array = [];
-    
-    console.log(palavraSelecionada);
-    
+    const [vermelho, setvermelho] = React.useState("");
+    const [chute, setchute] = React.useState("")
+    const [desabilitaChute, setdesabilitaChute] = React.useState(false);
+
+    function Chutar() {
+        const palavraString = palavraSelecionada.toString();
+        const stringSemVirgula = palavraString.replace(/,/g, "").replace(/\./g, "");
+        console.log(stringSemVirgula);
+        if (chute === stringSemVirgula) {
+            setlinhas(stringSemVirgula);
+            setvermelho("verde");
+            setdesabilitaChute(true);
+            return setdesabilitaButton(false);
+        }
+        else {
+            setlinhas(stringSemVirgula);
+            setvermelho("vermelho");
+            setfase(forca6);
+            setdesabilitaChute(true);
+            return setdesabilitaButton(false);
+        }
+    }
+
+    function removerCaracteres(str) {
+        str = str.toLowerCase();
+        str = str.replace(new RegExp('[ÁÀÂÃ]', 'gi'), 'a');
+        str = str.replace(new RegExp('[ÉÈÊ]', 'gi'), 'e');
+        str = str.replace(new RegExp('[ÍÌÎ]', 'gi'), 'i');
+        str = str.replace(new RegExp('[ÓÒÔÕ]', 'gi'), 'o');
+        str = str.replace(new RegExp('[ÚÙÛ]', 'gi'), 'u');
+        str = str.replace(new RegExp('[Ç]', 'gi'), 'c');
+        return str;
+    }
+
     function EscolherPalavra() {
-        
-        const escolhida = palavras[Math.floor(Math.random() * palavras.length)];
+
+        const escolhida = removerCaracteres(palavras[Math.floor(Math.random() * palavras.length)]);
         setpalavraSelecionada([...escolhida]);
+        console.log(escolhida);
         const traco = "_";
         for (let i = 0; i < [...escolhida].length; i++) {
             linhas.push(traco);
         }
-        arrayNovo = linhas;
-        setlinhas(arrayNovo);
-        
+        arrayDeLinhas = linhas;
+        setlinhas(arrayDeLinhas);
+
         const desablita = true;
         setdesabilitaButton(desablita);
-        
+
     }
-    
-    function Teclado(props) {
-        const [habilitaTeclado, sethabilitaTeclado] = React.useState(false);
-        const [cor, setcor] = React.useState("habilitado");
-        
-        function EscolherLetra(l) {
-            sethabilitaTeclado(true);
-
-            const mudaCor = "";
-            setcor(mudaCor);
-
-            palavraSelecionada.forEach((elemento, indice) => {
-                if (elemento === l) {
-                    linhas[indice] = palavraSelecionada[indice];
-                }
-                return linhas[indice];
-            });
-            if(!palavraSelecionada.includes(l)){
-                console.log("errou");
-                 seterro(erro + 1); 
-                if(erro === 1){
-                    setfase(forca1);
-                }
-                if(erro === 2){
-                    setfase(forca2);
-                }
-                if(erro === 3){
-                    setfase(forca3);
-                }
-                if(erro === 4){
-                    setfase(forca4);
-                }
-                if(erro === 5){
-                    setfase(forca5);
-                }
-                if(erro === 6){
-                    setfase(forca6);
-                }
-                if(erro === 7){
-                    alert("perdeu");
-                    setlinhas(palavraSelecionada);
-                }
-            }
-            array = linhas;
-            setlinhas(array);
-            console.log(array);
-           
-        }
-        
-        if (props.desabilitaButton === true) {
-            return (<button disabled={habilitaTeclado} className={`letra ${cor}`} onClick=
-                {() => EscolherLetra(props.l)}>{props.l}</button>);
-        }
-        else {
-            return (<button disabled className={`letra`}>{props.l}</button>);
-        }
-    
-    }
-
 
     return (
         <div className="jogo">
@@ -117,15 +151,17 @@ export default function App() {
             </div>
             <div className="teclado">
                 {alfabeto.map((l, letra) => <Teclado key={letra} l={l} desabilitaButton={desabilitaButton}
-                      />)}
+                    linhas={linhas} erro={erro} fase={fase} palavra={palavraSelecionada} seterro={seterro}
+                    setfase={setfase} setlinhas={setlinhas} arrayDeLetrasTrocadas={arrayDeLetrasTrocadas}
+                    setvermelho={setvermelho} setdesabilitaButton={setdesabilitaButton} />)}
             </div>
             <div className="chute">
                 <a className="negrito">Já sei a palavra!</a>
-                <input></input>
-                <button className="">Chutar</button>
+                <input placeholder="Pesquisar" value={chute} onChange={e => setchute(e.target.value)} />
+                <button className="" disabled={desabilitaChute} onClick={Chutar}>Chutar</button>
             </div>
             <div className="underline">
-                <a className="linhas">{linhas}</a>
+                <h1 className={`linhas ${vermelho}`}>{linhas}</h1>
             </div>
         </div>
     )
